808e4c041632682fc4421df7479fca4ba0968df8
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2024-09-15 00:23:20 +0800

    recover cpu settings first when cleanup

--- etc/libvirt/hooks/qemu.hooks.d/.scripts/cleanup.sh
+++ etc/libvirt/hooks/qemu.hooks.d/.scripts/cleanup.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-source "/etc/libvirt/hooks/qemu.hooks.d/.scripts/unbind_vfio.sh"
 source "/etc/libvirt/hooks/qemu.hooks.d/.scripts/reattachcpu.sh"
+source "/etc/libvirt/hooks/qemu.hooks.d/.scripts/unbind_vfio.sh"
